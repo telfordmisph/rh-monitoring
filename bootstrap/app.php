e05d6e461edd0cc3b37e5c55d3a7d05881f1b1f4
Emem <ememl@emem.com>
2026-03-09 08:15:20 +0800

    fix: auth middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -54,6 +54,7 @@ return Application::configure(basePath: dirname(__DIR__))
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
             \App\Http\Middleware\SessionMiddleware::class,
+            \App\Http\Middleware\AuthMiddleware::class,
         ]);
     })
     ->create();
